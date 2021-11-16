bd2fb4d910432738cfec8a0cfa8fa3e1892ffd19
oolawepo <olasubomiolawepo@gmail.com>
2021-11-15 19:43:10 -0600

    main

---
+++ contracts/Market.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 listing price = 0.025 ether;
+}
